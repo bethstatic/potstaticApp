8dcf90977c0faeb4b8e4493188e06a4c908f2f5d
bethstatic <61440795+bethstatic@users.noreply.github.com>
2021-04-15 22:54:26 -0300

    Add files via upload

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Pots',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+
+]
