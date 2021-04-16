3cc9cf06f75fca845e7aebe0abd70d29a6f7630c
Betsy Rumay <betsycrp@gmail.com>
2021-04-15 22:45:07 -0300

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'React';
+import React, { Component } from 'react';
 import { MenuItems } from "./MenuItems"
+import { Buttons } from "../Buttons"
 import './Navbar.css'
 
 class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
     render() {
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Potstatic<i className="fab fa-react"></i></h1>
-                <div className="menu-icon">
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
 
                 </div>
-                <ul>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                     {MenuItems.map((item, index)=> {
                         return (
                             <li key={index}>
@@ -22,6 +30,7 @@ class Navbar extends Component {
                     })}
                     
                 </ul>
+                <Buttons>Sing Up</Buttons>
             </nav>
         )
     }
